83685d1fd9a801e449a750530a601ae002c28dc9
ElliotAlderson-cmd <prajwalghotage2508@gmail.com>
2024-04-07 12:44:38 +0530

    Updated UI

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ from contact_us import *
 import warnings
 warnings.filterwarnings('ignore')
 
-st.title('PREDICTIFY - Used Car Data Analysis')
+
 
 #loading files for animations
 def load_lottiefiles(filepath: str):
     with open(filepath, 'r') as f:
         return json.load(f)
 
-lottie_graph = load_lottiefiles('animations/anim_graphs.json')
+lottie_graph = load_lottiefiles('animations/data-analysis-gif.json')
 lottie_car = load_lottiefiles('animations/car.json')
 lottie_car2 = load_lottiefiles('animations/car2.json')
 
 st.sidebar.title('Navigation Panel')
-nav = st.sidebar.radio(' ',['Analyze', 'Prediction', 'Contact Us'])
+nav = st.sidebar.radio(' ',['Analyze', 'Prediction'])
 
 if nav == 'Analyze':
+    st.title('Used Car Data Analysis and Visualisation')
     st_lottie(
     lottie_graph,
     quality= 'medium',
@@ -32,15 +33,13 @@ if nav == 'Analyze':
     graphs()
 
 if nav == 'Prediction':
+    st.title('Price Prediction using Machine Learning Algorithms')
     st_lottie(
-    lottie_car2,
+    lottie_car,
     quality= 'medium',
     width= '40rem',
     height= '15rem',
     )
     predict_data()
-    
-if nav == 'Contact Us':
-    contact()
         
     
